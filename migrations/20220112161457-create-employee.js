0442a49c00f8536532c76e3286258dc105b5a0e1
Yaosabi <lorenzoimurillo@outlook.com>
2022-01-13 08:17:12 -0800

    initial commit

---
+++ migrations/20220112161457-create-employee.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstname: {
+        type: Sequelize.STRING
+      },
+      lastname: {
+        type: Sequelize.STRING
+      },
+      jobtitle: {
+        type: Sequelize.STRING
+      },
+      streetline1:{
+        type: Sequelize.STRING
+      },
+      streetline2:{
+        type: Sequelize.STRING
+      },
+      city:{
+        type: Sequelize.STRING
+      },
+      state:{
+        type: Sequelize.STRING
+      },
+      zip:{
+        type: Sequelize.INTEGER
+      },
+      phonenumber:{
+        type: Sequelize.INTEGER
+      },
+      yearhired:{
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Employees');
+  }
+};
