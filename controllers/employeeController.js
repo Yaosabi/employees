059319b717b58ef56bce93fa5bed348ccd1c3551
Yaosabi <lorenzoimurillo@outlook.com>
2022-01-17 15:32:13 -0800

    checkpoint 2

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -10,6 +10,18 @@ module.exports.displayEmployees = async function(req, res){
 module.exports.renderAddEmployeeForm = function(req, res){
     res.render('createUserForm',
         {
+            employee:{
+                firstname:'',
+                lastname:'',
+                jobtitle:jobtitles[0],
+                streetline1:'',
+                streetline2:'',
+                city:'',
+                state:states[0],
+                zip:'',
+                phonenumber:'',
+                yearhired:''
+            },
             jobtitles,
             statelist:states
         });
@@ -27,8 +39,48 @@ module.exports.addEmployee = async function(req, res){
             state:req.body.state,
             zip:req.body.zip,
             phonenumber:req.body.phonenumber,
-            yearhired:req.body.yearhired,
+            yearhired:req.body.yearhired
         }
     );
+    res.redirect('/');
+}
+
+module.exports.renderUpdateForm = async function(req, res){
+    const employee = await Employee.findByPk(req.params.id);
+    res.render('edit', {
+        employee,
+        jobtitles,
+        stateslist:states
+    });
+}
+
+module.exports.updateEmployee = async function(req, res){
+    await Employee.update(
+        {
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            jobtitle:req.body.jobtitle,
+            streetline1:req.body.streetline1,
+            streetline2:req.body.streetline2,
+            city:req.body.city,
+            state:req.body.state,
+            zip:req.body.zip,
+            phonenumber:req.body.phonenumber,
+            yearhired:req.body.yearhired
+        },
+        {
+            where:{
+                id:req.params.id
+            }
+        });
+    res.redirect('/');
+}
+
+module.exports.deleteEmployee = async function(req,res){
+    await Employee.destroy({
+        where:{
+            id:req.params.id
+        }
+    });
     res.redirect('/');
 }
